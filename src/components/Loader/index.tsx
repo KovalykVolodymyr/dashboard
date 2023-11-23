f0cbfb2d95f23bb0537101ea453c7908e2e80d06
KovalykVolodymyr <kovalykvovapl@gmail.com>
2023-11-23 14:31:36 +0200

    update Loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -2,7 +2,16 @@ import Skeleton from 'components/Skeleton'
 import { FC } from 'react'
 
 const Loader: FC = () => {
-  return <Skeleton width="100%" height={300} spaceBottom={16} />
+  return (
+    <Skeleton
+      width="100%"
+      height={500}
+      animation="wave"
+      type="block"
+      layout="rect"
+      spaceBottom={16}
+    />
+  )
 }
 
 export default Loader
